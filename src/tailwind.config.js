f74de82e3406c8e28d2d4ac8ead545396ae363cb
Ivan Szebenszki <szib77@gmail.com>
2019-08-05 08:45:17 +0200

    Change color, remove other colors, fonts

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        color1: 'rgba(20, 36, 45, 1)',
-        color2: 'rgba(18, 22, 25, 1)',
-        color3: 'rgba(15, 38, 61, 1)',
-        color4: 'rgba(25, 100, 126, 1)',
-        color5: 'rgba(231, 119, 40, 1)',
-      },
-      fontFamily: {
-        display: ['Orbitron', 'sans-serif'],
-      },
+    colors: {
+      transparent: 'transparent',
+      color1: 'rgba(20, 36, 45, 1)',
+      color2: 'rgba(18, 22, 25, 1)',
+      color3: 'rgba(15, 38, 61, 1)',
+      color4: 'rgba(30, 151, 191, 1)',
+      color5: 'rgba(231, 119, 40, 1)',
     },
+    fontFamily: {
+      display: ['Orbitron', 'sans-serif'],
+    },
+    extend: {},
   },
   variants: {},
   plugins: [],
