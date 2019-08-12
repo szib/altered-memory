1fdebaff2ae261f3818d061fa93bfb1fa5028f02
Ivan Szebenszki <szib77@gmail.com>
2019-08-12 20:13:32 +0200

    Design about and landing page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,54 +1,46 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import tw from 'tailwind.macro';
 
 import Button from '../components/Button';
-
-const Screen = tw.div`
-  bg-color1 
-  w-screen h-screen 
-  flex justify-center items-center
-  `;
-
-const Container = tw.div`
-  h-full w-full
-  m-20 
-  flex flex-col justify-center items-center
-`;
-
-const Title = tw.div`
-  text-color5 
-  font-display
-  text-6xl 
-  text-center
-  mb-6
-  `;
-
-const Text = tw.div`
-  text-color5 
-  font-display
-  text-2xl 
-  text-center
-  my-2
-`;
+import * as TW from '../components/TW';
+import { Github, LinkedIn } from '../components/Icons';
 
 const LandingPage = () => {
   return (
-    <Screen>
-      <Container>
-        <Title>Altered Memory</Title>
-        <Text>
+    <TW.Screen>
+      <TW.Container>
+        <TW.Title>Altered Memory</TW.Title>
+        <TW.Text>
           Originally this game was a Mod3 project while I was doing Software
-          Engineering bootcamp at Flatiron School.
-        </Text>
-        <Text>
+          Engineering bootcamp at Flatiron School. That was a pure Javascript
+          game and in five days we had no time to include all the features we
+          wanted.
+        </TW.Text>
+        <TW.Text>
+          After graduation I rewrote the game - almost - from scratch using
+          React. The code is not public at the moment, but I might make it open
+          source in the future. For now you can have a look on the code of the
+          &nbsp;
+          <TW.A href="http://github.com/szib/altered-memory-frontend">
+            original game.
+          </TW.A>
+        </TW.Text>
+
+        <TW.Title>Author</TW.Title>
+        <TW.Text>Ivan Szebenszki</TW.Text>
+
+        <TW.Text>
+          <Github />
+          <LinkedIn />
+        </TW.Text>
+        <TW.Text>
           <Link to="/" as={Button}>
             <Button>Back</Button>
           </Link>
-        </Text>
-      </Container>
-    </Screen>
+        </TW.Text>
+      </TW.Container>
+    </TW.Screen>
   );
 };
 
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,67 +1,37 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import tw from 'tailwind.macro';
 
 import Button from '../components/Button';
 
-const Screen = tw.div`
-  bg-color1 
-  w-screen h-screen 
-  flex justify-center items-center
-  `;
-
-const Container = tw.div`
-  h-full w-full
-  m-20 
-  flex flex-col justify-center items-center
-`;
-
-const Title = tw.div`
-  text-color5 
-  font-display
-  text-6xl 
-  text-center
-  mb-6
-  `;
-
-const Text = tw.div`
-  text-color5 
-  font-display
-  text-2xl 
-  text-center
-  my-2
-`;
-
-const SmallText = tw(Text)`
-  text-sm
-  uppercase
-`;
+import * as TW from '../components/TW';
 
 const LandingPage = () => {
   return (
-    <Screen>
-      <Container>
-        <Title>Altered Memory</Title>
-        <Text>It's not rocket science.</Text>
-        <Text>Just memorize them.</Text>
-        <Text>Then find them.</Text>
-        <Text>Oh. One more thing...</Text>
-        <Text>You really do not want to make mistakes...</Text>
-        <Text>
+    <TW.Screen>
+      <TW.Container>
+        <TW.Title>Altered Memory</TW.Title>
+        <TW.Text>It's not rocket science.</TW.Text>
+        <TW.Text>Just memorize them.</TW.Text>
+        <TW.Text>Then find them.</TW.Text>
+        <TW.Text>Oh. One more thing...</TW.Text>
+        <TW.Text>You really do not want to make mistakes...</TW.Text>
+        <TW.Text>
           <Link to="/game" as={Button}>
             <Button>Start game</Button>
           </Link>
-        </Text>
-        <SmallText>
-          We use cookies. Close the page now if you're not happy with this.
-        </SmallText>
-        <SmallText>
-          We are in development mode, so anything could happen.
-        </SmallText>
-        <SmallText>And the exact opposite too.</SmallText>
-      </Container>
-    </Screen>
+        </TW.Text>
+        <TW.Text>
+          <Link to="/About" as={Button}>
+            <Button>About</Button>
+          </Link>
+        </TW.Text>
+        <TW.SmallText>
+          We use cookies. Close the page now if you're not happy with this. No
+          harm done so far...
+        </TW.SmallText>
+      </TW.Container>
+    </TW.Screen>
   );
 };
 
