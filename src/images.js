d9980bc6b7dd555e0db8c05a5520f29079d35e54
Ivan Szebenszki <szib77@gmail.com>
2019-08-24 10:55:39 +0200

    Add tech icons

--- src/images.js
+++ src/images.js
@@ -12,13 +12,13 @@ const original = importAll(
   require.context('./assets/images/original', false, /\.(png|jpe?g|svg)$/)
 );
 
-const sports = importAll(
-  require.context('./assets/images/sports', false, /\.(png|jpe?g|svg)$/)
+const tech = importAll(
+  require.context('./assets/images/tech', false, /\.(png|jpe?g|svg)$/)
 );
 
 export const cardImages = {
   original,
-  sports,
+  tech,
 };
 
 export { backImage };
